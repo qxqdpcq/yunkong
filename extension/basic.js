89b715ac244d05a41bb67c821132667deefbb455
qxqdpcq <85611284+qxqdpcq@users.noreply.github.com>
2022-10-01 14:23:11 +0800

    Update basic.js

--- extension/basic.js
+++ extension/basic.js
@@ -1077,7 +1077,7 @@ window.YKimport(function(lib,game,ui,get,ai,_status){
 	game.YKgainNewPerson=function(characterx,bool){
 		if(Array.isArray(characterx)){character=characterx[0];characterx=characterx.slice(1,characterx.length);}
 		if(characterx.length<=1) bool=true;
-		if(!character){alert('角色名错误！');return ;}
+		if(!characterx){alert('角色名错误！');return ;}
 		if(typeof game.ykHasCharacter=='function'&&game.ykHasCharacter(character)){alert('您已拥角色【'+get.translation(character.slice(0,character.indexOf('-')))+'】！');return ;}
 		if(typeof character!='string') var character=''+character;
 		if(character.indexOf('qxq_yk')==-1||character.indexOf('-')==-1){
