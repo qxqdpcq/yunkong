ade6123f38be3d2e4aafb5e86fc3b8fcd02eb239
qxqdpcq <85611284+qxqdpcq@users.noreply.github.com>
2022-10-07 08:35:30 +0800

    Update ExYKPerson.js

--- extension/ExYKPerson.js
+++ extension/ExYKPerson.js
@@ -10288,8 +10288,12 @@ var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68
 				}
 				if((lib.config['yk_ykzhuyan_rank']||0)>=3){
 					trigger._triggered=null;
-					trigger.untrigger(true);
+					trigger._notrigger=game.players;
 					trigger.finish();
+					trigger._triggered=5;
+					if(trigger._triggering.map) for(var evt of trigger._triggering.map){
+						if(evt.player==player) evt.list.length=0;
+					};
 				}
 				game.delay(2);
 			}
