26e6cccf3ccb7b58c8f9849103e8d2edfa92ee77
qxqdpcq <85611284+qxqdpcq@users.noreply.github.com>
2022-10-11 20:14:40 +0800

    Update ExYKPerson.js

--- extension/ExYKPerson.js
+++ extension/ExYKPerson.js
@@ -1578,6 +1578,7 @@ var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68
 					return;
 				}
 				juqing_while();
+						List[Listnum].end_fuc(List[Listnum].avatar,List.person);
 				num = 0;
 				avatarshow();
 			}
