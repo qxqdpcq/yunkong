672cee3d7bdb081a66b003678e67a0144aba834a
qxqdpcq <85611284+qxqdpcq@users.noreply.github.com>
2022-10-11 20:24:25 +0800

    Update ExYKPerson.js

--- extension/ExYKPerson.js
+++ extension/ExYKPerson.js
@@ -1500,6 +1500,10 @@ var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68
 		var avatar;
 		var juqing_while = function() {
 			for (var i in List[Listnum]) {
+				if(juqing_while.end_fuc){
+					delete juqing_while.end_fuc_obj;
+					juqing_while.end_fuc_obj=null;
+				}
 				switch (i) {
 					case 'say':
 						str = List[Listnum][i];
@@ -1514,6 +1518,9 @@ var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68
 						break;
 					case 'fuc':
 						List[Listnum][i](avatar,List.person);
+						break;
+					case 'end_fuc':
+						juqing_while.end_fuc_obj={obj:List[Listnum][i],person:List.person};
 						break;
 					case 'backgroundAudio':
 						if (ui.window.contains(juqing_audio)) {
@@ -1578,7 +1585,7 @@ var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68
 					return;
 				}
 				juqing_while();
-						List[Listnum].end_fuc(List[Listnum].avatar,List.person);
+					if(juqing_while.end_fuc_obj) juqing_while.end_fuc_obj.obj.end_fuc(juqing_while.end_fuc_obj.obj.avatar,juqing_while.end_fuc_obj.obj.person);
 				num = 0;
 				avatarshow();
 			}
