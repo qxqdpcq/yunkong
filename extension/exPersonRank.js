ee7b6420c4b8f2b26bdd1f7b8378a008e375c267
qxqdpcq <85611284+qxqdpcq@users.noreply.github.com>
2022-10-21 19:03:24 +0800

    Update exPersonRank.js

--- extension/exPersonRank.js
+++ extension/exPersonRank.js
@@ -8,5 +8,5 @@ window.YKimport(function(lib,game,ui,get,ai,_status){
 	//地级
 	lib.rank.rarity.epic.addArray([]);
 	//天级、超稀
-	lib.rank.rarity.legend.addArray(["qxq_yk_tian","qxq_yk_xiaoqiao","qxq_yk_yanmengyuejian","qxq_yk_kongshanlingxue"]);
+	lib.rank.rarity.legend.addArray(["qxq_yk_tian","qxq_yk_xiaoqiao","qxq_yk_yanmengyuejian","qxq_yk_kongshanlingxue","qxq_yk_mingyun"]);
 });
